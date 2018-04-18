1ef52a0411709d1efcb09e1eb305ed0396f615ec
Jongheon94 <ismir1449@gmail.com>
2018-04-18 19:10:14 +0900

    mexampractice1
    insert추가

---
+++ miexam-practice/src/main/java/kr/ac/jejunu/ProductDao.java
@@ -0,0 +1,51 @@
+package kr.ac.jejunu;
+
+import java.sql.*;
+
+public class ProductDao {
+    public Product get(Long id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
+        preparedStatement.setLong(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        Product product = new Product();
+        product.setId(resultSet.getLong("id"));
+        product.setTitle(resultSet.getString("title"));
+        product.setPrice(resultSet.getInt("price"));
+
+        //자원을 해지한다.
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return product;
+    }
+    public Long insert(Product product) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into product(title, price)  value(?,?) ");
+        preparedStatement.setString(1, product.getTitle());
+        preparedStatement.setInt(2, product.getPrice());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement( "select last_insert_id()" );
+        ResultSet resultSet = preparedStatement.executeQuery(); //반환값 가져온 걸 리턴하여 resultSet에 넣음
+        resultSet.next();
+
+        Long id = resultSet.getLong(1);
+
+        //자원을 해지한다.
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+}
---
+++ miexam-practice/src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -0,0 +1,43 @@
+package kr.ac.jejunu;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ProductDaoTest {
+    ProductDao productDao;
+
+    @Before
+    public void setup() {
+        productDao = new ProductDao();
+    }
+
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+
+        Long id = 1L;
+        String title = "제주감귤";
+        Integer price = 15000;
+
+        Product product = productDao.get(id);
+        assertEquals(id, product.getId());
+        assertEquals(title, product.getTitle());
+        assertEquals(price, product.getPrice());
+    }
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Product product = new Product();
+        product.setTitle( "제주오메기떡" );
+        product.setPrice( 15000 );
+        Long id = productDao.insert( product );
+
+        Product insertedProduct = productDao.get( id );
+        assertEquals(insertedProduct.getId(), id);
+        assertEquals(insertedProduct.getTitle(), product.getTitle());
+        assertEquals(insertedProduct.getPrice(), product.getPrice());
+    }
+}
