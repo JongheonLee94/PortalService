346619c7af5d9ceded9e9e0b61d556b1672484e5
Jongheon94 <ismir1449@gmail.com>
2018-04-19 23:30:21 +0900

    mexam-9-1
    dao에서 비슷한 부분이 많고 일부분이 다르니 추상화 인터페이스하여
    prestatement부분을 바꿈

--- miexam-practice/src/main/java/kr/ac/jejunu/ProductDao.java
+++ miexam-practice/src/main/java/kr/ac/jejunu/ProductDao.java
@@ -32,9 +32,9 @@ public class ProductDao {
                 product.setPrice( resultSet.getInt( "price" ) );
             }
         } finally {
-            //자원을 해지한다. 해지하지 않게 되면 쌓여서 시스템이 다운될 수 있으므로 반드시 해지해야하므로 final로 해준다.
-            if (resultSet != null) {//resultSet이 null이면 해지할 게 없으므로
-                try {//close에서 에러가 날 수도 있으므로
+            //자원을 해지한다.
+            if (resultSet != null) {
+                try {
                     resultSet.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
@@ -49,7 +49,7 @@ public class ProductDao {
             }
             if (connection != null) {
                 try {
-                    connection.close(); //실제로는 커넥션만 close 되면 다 close 된다고 함
+                    connection.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -65,7 +65,7 @@ public class ProductDao {
     public Long insert(Product product) throws ClassNotFoundException, SQLException {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null; //반환값 가져온 걸 리턴하여 resultSet에 넣음
+        ResultSet resultSet = null;
         Long id;
         try {
             connection = dataSource.getConnection();
@@ -117,11 +117,8 @@ public class ProductDao {
 
         try {
             connection = dataSource.getConnection();
-
-            preparedStatement = connection.prepareStatement( "update product set title = ?, price = ? where id= ? " );
-            preparedStatement.setString( 1, product.getTitle() );
-            preparedStatement.setInt( 2, product.getPrice() );
-            preparedStatement.setLong( 3, product.getId() );
+            StatementStrategy statementStrategy =new UpdateProductStatementStrategy(product);
+            preparedStatement = statementStrategy.makeStatement( connection );
 
             preparedStatement.executeUpdate();
 
@@ -144,15 +141,16 @@ public class ProductDao {
         }
     }
 
+
     public void delete(Long id) throws SQLException {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
 
         try {
             connection = dataSource.getConnection();
+            StatementStrategy statementStrategy = new DeleteProductStamentStrategy(id);
+            preparedStatement =statementStrategy.makeStatement( connection );
 
-            preparedStatement = connection.prepareStatement( "delete from product where id = ?" );
-            preparedStatement.setLong( 1,id );
 
             preparedStatement.executeUpdate();
 
