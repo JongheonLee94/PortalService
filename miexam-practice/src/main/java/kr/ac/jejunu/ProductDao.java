8f424cf85d8a91df413237be52404e8bf5fd542e
Jongheon94 <ismir1449@gmail.com>
2018-04-20 01:10:36 +0900

    mexam 13-1
    Jdbc Context 를 JdbcTemplet으로 변경

--- miexam-practice/src/main/java/kr/ac/jejunu/ProductDao.java
+++ miexam-practice/src/main/java/kr/ac/jejunu/ProductDao.java
@@ -1,38 +1,40 @@
 package kr.ac.jejunu;
 
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.sql.*;
 
 public class ProductDao {
+    private  final JdbcTemplate jdbcTemplate;
 
-    private JdbcContext jdbcContext;
 
-    public ProductDao(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public ProductDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public Product get(Long id) throws SQLException {
         String sql = "select * from product where id = ?";
         Object[] params = new Object[]{id};
-        return jdbcContext.queryForObject( sql, params );
+        return jdbcTemplate.queryForObject( sql, params );
     }
 
     public Long insert(Product product) throws SQLException {
         String sql = "insert into product(title, price)  value(?,?) ";
         Object[] params = new Object[]{product.getTitle(), product.getPrice()};
-        return jdbcContext.insert( sql, params );
+        return jdbcTemplate.insert( sql, params );
     }
 
     public void uppdate(Product product) throws SQLException {
         String sql = "update product set title = ?, price = ? where id= ? ";
         Object[] params = new Object[]{product.getTitle(), product.getPrice(), product.getId()};
-        jdbcContext.update( sql, params );
+        jdbcTemplate.update( sql, params );
     }
 
     public void delete(Long id) throws SQLException {
         String sql = "delete from product where id = ?";
         Object[] params = new Object[]{id};
-        jdbcContext.update( sql, params );
+        jdbcTemplate.update( sql, params );
     }
 
 }
