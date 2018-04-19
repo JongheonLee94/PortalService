d2156c5a0d315447b07c06d1f7d0f793f74db27b
Jongheon94 <ismir1449@gmail.com>
2018-04-19 12:43:39 +0900

    exam 7
    서버에 장애가 생겼다. close가 안되서 그런거 같다 할 때 try final try catch if

--- miexam-practice/src/main/java/kr/ac/jejunu/ProductDao.java
+++ miexam-practice/src/main/java/kr/ac/jejunu/ProductDao.java
@@ -32,10 +32,28 @@ public class ProductDao {
             product.setTitle(resultSet.getString("title"));
             product.setPrice(resultSet.getInt("price"));
         } finally {
-            //자원을 해지한다.
-            resultSet.close();
-            preparedStatement.close();
-            connection.close();
+            //자원을 해지한다. 해지하지 않게 되면 쌓여서 시스템이 다운될 수 있으므로 반드시 해지해야하므로 final로 해준다.
+            if (resultSet != null) {//resultSet이 null이면 해지할 게 없으므로
+                try {//close에서 에러가 날 수도 있으므로
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close(); //실제로는 커넥션만 close 되면 다 close 된다고 함
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
 
@@ -45,24 +63,50 @@ public class ProductDao {
 
 
     public Long insert(Product product) throws ClassNotFoundException, SQLException {
-        Connection connection = dataSource.getConnection();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null; //반환값 가져온 걸 리턴하여 resultSet에 넣음
+        Long id;
+        try {
+            connection = dataSource.getConnection();
 
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into product(title, price)  value(?,?) ");
-        preparedStatement.setString(1, product.getTitle());
-        preparedStatement.setInt(2, product.getPrice());
+            preparedStatement = connection.prepareStatement("insert into product(title, price)  value(?,?) ");
+            preparedStatement.setString(1, product.getTitle());
+            preparedStatement.setInt(2, product.getPrice());
 
-        preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
 
-        preparedStatement = connection.prepareStatement( "select last_insert_id()" );
-        ResultSet resultSet = preparedStatement.executeQuery(); //반환값 가져온 걸 리턴하여 resultSet에 넣음
-        resultSet.next();
+            preparedStatement = connection.prepareStatement( "select last_insert_id()" );
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
 
-        Long id = resultSet.getLong(1);
+            id = resultSet.getLong(1);
+        } finally {
+            //자원을 해지한다.
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-        //자원을 해지한다.
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+
 
         return id;
     }
