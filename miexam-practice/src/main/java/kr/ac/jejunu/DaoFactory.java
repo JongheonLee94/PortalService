4a21d179c006fc014bcf2f2d44b3000f91ea1f36
Jongheon94 <ismir1449@gmail.com>
2018-04-19 10:43:41 +0900

    exam 6
    datasource

--- miexam-practice/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ miexam-practice/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -2,15 +2,34 @@ package kr.ac.jejunu;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
 
 @Configuration
 public class DaoFactory {
+    private String className= "com.mysql.jdbc.Driver" ;
+    private String url="jdbc:mysql://localhost/jeju";
+    private String userName="jeju" ;
+    private String password="jejupw" ;
     @Bean
     public ProductDao productDao(){
-        return new ProductDao( connection() );
+        return new ProductDao( datasource() );
     }
     @Bean
-    public ConnectionMaker connection() {
-        return new JejuConnectionMaker();
+    public DataSource datasource()
+    {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource(  );
+        try {
+            dataSource.setDriverClass( (Class<? extends Driver>) Class.forName( className ) );
+        } catch (ClassNotFoundException e) {
+            new RuntimeException( e );
+        }
+        dataSource.setUrl( url );
+        dataSource.setUsername( userName );
+        dataSource.setPassword( password );
+        return dataSource;
     }
 }
