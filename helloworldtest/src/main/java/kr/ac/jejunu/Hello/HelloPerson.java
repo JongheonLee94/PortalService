14877d6f40e0bbf519ebf25f3f21b92800a0474e
Jongheon94 <ismir1449@gmail.com>
2018-06-04 23:12:08 +0900

    AnnotationConfigApplicationContext3
    Construct를 lombok을 이용해 Annotation으로 생성함

--- helloworldtest/src/main/java/kr/ac/jejunu/Hello/HelloPerson.java
+++ helloworldtest/src/main/java/kr/ac/jejunu/Hello/HelloPerson.java
@@ -1,21 +1,20 @@
 package kr.ac.jejunu.Hello;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
+//@AllArgsConstructor      //이건 전부 construct로
+@RequiredArgsConstructor   //final필드에 있는 애만 construct로 만들어주고 autowired까지 해줌
 public class HelloPerson implements Hello {
     @Value( "허윤호" )
     private  String name;
     private  final Hello hello;
-
-    @Autowired
-    public HelloPerson(Hello hello){ //실수를 줄이기 위해 명시적으로 construct를 만드는 게 일반적이라고 한다.
-        this.hello = hello;
-    }
 
     public String sayHello(){
         return hello.sayHello()+ name;
