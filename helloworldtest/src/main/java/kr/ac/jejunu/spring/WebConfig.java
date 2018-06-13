dbcf61726223da445092554f9fce46c1746603e9
Jongheon94 <ismir1449@gmail.com>
2018-06-13 17:42:54 +0900

    config class로
    configureViewResolvers메소드 이용
    jsp , contentNegotiation resolver 꽂음.

--- helloworldtest/src/main/java/kr/ac/jejunu/spring/WebConfig.java
+++ helloworldtest/src/main/java/kr/ac/jejunu/spring/WebConfig.java
@@ -3,10 +3,19 @@ package kr.ac.jejunu.spring;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
 
 @Configuration
 @EnableWebMvc   //annotation-driven과 같은효과
 @ComponentScan(basePackages = "kr.ac.jejunu")  //<context:component-scan base-package="kr.ac.jejunu"/>과 같은 것
-public class WebConfig {
-
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp( "/WEB-INF/views/", ".jsp" );
+        registry.enableContentNegotiation( new MappingJackson2JsonView() );
+        registry.enableContentNegotiation( new MappingJackson2XmlView() );
+    }
 }
