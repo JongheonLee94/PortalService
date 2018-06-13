60a871292cc00cee9c11fb01795b5632a63f2faa
Jongheon94 <ismir1449@gmail.com>
2018-06-13 19:41:52 +0900

    addResourceHandlers
    리소스핸들러로 이미지 띄우는 것  자바코드로

--- helloworldtest/src/main/java/kr/ac/jejunu/spring/WebConfig.java
+++ helloworldtest/src/main/java/kr/ac/jejunu/spring/WebConfig.java
@@ -2,9 +2,8 @@ package kr.ac.jejunu.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
 
@@ -12,6 +11,17 @@ import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
 @EnableWebMvc   //annotation-driven과 같은효과
 @ComponentScan(basePackages = "kr.ac.jejunu")  //<context:component-scan base-package="kr.ac.jejunu"/>과 같은 것
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler( "/images/**" ).addResourceLocations( "/WEB-INF/static/" );
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.mediaType( "js", MediaType.APPLICATION_JSON ); // js=application/json
+        configurer.mediaType( "x",MediaType.APPLICATION_XML ); // x=application/xml
+    }
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp( "/WEB-INF/views/", ".jsp" );
