72aaa2c7cf9e0cd78cf623f909df8c8a7085ff23
Jongheon94 <ismir1449@gmail.com>
2018-06-13 20:05:21 +0900

    multipartResolver
    파일업로드를 위해 multipartResolver를 꽂음. enableMvc에 기본으로 되어있다고 들었는데 안되는 이유를 찾아야함.

--- helloworldtest/src/main/java/kr/ac/jejunu/spring/WebConfig.java
+++ helloworldtest/src/main/java/kr/ac/jejunu/spring/WebConfig.java
@@ -1,8 +1,11 @@
 package kr.ac.jejunu.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
@@ -17,6 +20,11 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor( new HelloInterceptor() );
+    }
+
+    @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.mediaType( "js", MediaType.APPLICATION_JSON ); // js=application/json
         configurer.mediaType( "x",MediaType.APPLICATION_XML ); // x=application/xml
@@ -27,5 +35,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.jsp( "/WEB-INF/views/", ".jsp" );
         registry.enableContentNegotiation( new MappingJackson2JsonView() );
         registry.enableContentNegotiation( new MappingJackson2XmlView() );
+    }
+    @Bean
+    public MultipartResolver multipartResolver() { // enableWebMvc에 기본적으로 multipartResover가 포함된다고 했지만 되지 않아 꽂아야 했다.
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(10485760); // 10MB
+        multipartResolver.setMaxUploadSizePerFile(1048576); // 1MB
+        return multipartResolver;
     }
 }
